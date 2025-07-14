ae88deda0d22e6f9c24ed09b1c3c16212574bb78
doradora58 <sheerlifechallenge@gmail.com>
2025-07-15 01:55:37 +0900

    Refactoring

--- backend/WebApplication1/Program.cs
+++ backend/WebApplication1/Program.cs
@@ -1,31 +1,17 @@
 using System.Net;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        var localIp = GetLocalIPv4();
-        CreateHostBuilder(args, localIp).Build().Run();
+        CreateHostBuilder(args).Build().Run();
     }
 
-    public static IHostBuilder CreateHostBuilder(string[] args, string ip) =>
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseUrls("http://0.0.0.0:5000");
                 webBuilder.UseStartup<Startup>();
             });
-
-    private static string GetLocalIPv4()
-    {
-        var host = Dns.GetHostEntry(Dns.GetHostName());
-        foreach (var ip in host.AddressList)
-        {
-            if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
-                return ip.ToString();
-        }
-        throw new Exception("IPv4 アドレスが見つかりませんでした。");
-    }
 }
