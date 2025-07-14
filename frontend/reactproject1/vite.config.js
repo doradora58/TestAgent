d8f60e8439ce539175a223cfeb6c48e2f4301071
doradora58 <sheerlifechallenge@gmail.com>
2025-07-15 03:44:57 +0900

    a

--- frontend/reactproject1/vite.config.js
+++ frontend/reactproject1/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    build: {
+        outDir: 'dist', // これが別のフォルダになっていないか確認
+        emptyOutDir: true
+    },
     plugins: [react()],
     server: {
         host: '0.0.0.0',
