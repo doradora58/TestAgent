63d2fcdb616f586e8b15d77073f0cc6ca1d85c7f
doradora58 <sheerlifechallenge@gmail.com>
2025-07-24 02:59:30 +0900

    Fix

--- frontend/ReactApp/src/api/tasksApi.js
+++ frontend/ReactApp/src/api/tasksApi.js
@@ -1,5 +1,9 @@
 export async function fetchTasks() {
-    const response = await fetch("http://webapi:50000/Tasks");
+    const response = await fetch("http://webapi:50000/Tasks", {
+        headers: {
+            accept: "*/*",
+        },
+    });
     if (!response.ok) {
         throw new Error("Failed to fetch tasks");
     }
