181c701f6580ece7832bd63cec05addfdf0223a7
doradora58 <86960730+doradora58@users.noreply.github.com>
2025-07-24 09:24:44 +0900

    Update tasksApi.js

--- frontend/ReactApp/src/api/tasksApi.js
+++ frontend/ReactApp/src/api/tasksApi.js
@@ -1,5 +1,5 @@
 export async function fetchTasks() {
-    const response = await fetch("http://0.0.0.0:50000/Tasks", {
+    const response = await fetch("http://webapi:50000/Tasks", {
         headers: {
             "Content-Type": "application/json",
             "Accept": "*/*",
